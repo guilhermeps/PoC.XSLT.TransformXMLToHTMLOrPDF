9b59efa3bd1542993c7daba7dd4f18eb6eb37f22
Guilherme de Paula Silva <guilhermeps@ciandt.com>
2018-12-18 12:40:00 -0200

    Creation of solution file and project for transform XML to HTML using XSLT language.

---
+++ src/PoC.XSLT.TransformXMLToHTML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PoC.XSLT.TransformXMLToHTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
